f81f3ab6408aedf4b79cc8a1db168383733fadf1
unknown <kimjung111222@gmail.com>
2025-01-12 21:12:40 +0900

    mysql 5.7.44

---
+++ src/main/java/com/kang/nenpi/NenpiApplication.java
@@ -0,0 +1,13 @@
+package com.kang.nenpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NenpiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NenpiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kang/nenpi/NenpiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kang.nenpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NenpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
